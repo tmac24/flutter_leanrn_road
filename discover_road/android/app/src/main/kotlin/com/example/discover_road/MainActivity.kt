ef24eda3961efc365318eb4168d040d9b3e42fb4
孙涛 <727924283@qq.com>
2022-10-13 10:46:44 +0800

    初始化项目

---
+++ discover_road/android/app/src/main/kotlin/com/example/discover_road/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.discover_road
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ learn_road/android/app/src/main/kotlin/com/example/learn_road/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learn_road
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
